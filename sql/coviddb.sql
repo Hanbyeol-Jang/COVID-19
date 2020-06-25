47d8774868174524d74f0ae7428fcc53ed33d3d8
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2020-06-25 23:51:02 +0900

    Front-end React

--- sql/coviddb.sql
+++ sql/coviddb.sql
@@ -18,3 +18,15 @@ VALUES('admin', 'admin', 'admin', '2020-01-01', 'male', 'admin@admin.com', '010-
 
 commit;
 
+CREATE TABLE `covid_overview` (
+	`confirmed`		int 		NOT NULL,		# 확진자
+    `recovered`		int 		NOT NULL,		# 격리해제
+    `active`		int 		NOT NULL,		# 격리중
+	`deaths`		int 		NOT NULL,		# 사장
+    `date`			varchar(20) NOT NULL
+);
+
+INSERT INTO covid_overview(confirmed, recovered, active, deaths, date)
+VALUES(12563, 10974, 1307, 282, '2020-06-25');
+
+commit;
